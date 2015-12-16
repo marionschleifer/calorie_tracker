e356a787352324a51601c9c32ea7213f5140d22b
Marion Schleifer <marion.schleifer@simplificator.com>
2015-12-16 14:38:33 +0100

    migration to set position in entries
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109133230) do
+ActiveRecord::Schema.define(version: 20151216104513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151109133230) do
     t.datetime "updated_at", null: false
     t.date     "date"
     t.integer  "user_id",    null: false
+    t.integer  "position",   null: false
   end
 
   create_table "users", force: :cascade do |t|
